0d54a5a25f496d36b8cd8ccf8d18cb408b48ab87
BeaconBrigade <BeaconBrigade@gmail.com>
2022-05-16 16:13:19 -0230

    Created more clear functions

--- display.py
+++ display.py
@@ -1,10 +1,22 @@
 import guizero as gui
 app = gui.App("this is an app")
+
+def initGui() :
+	""" Start displaying the gui """
+	app.show()
+	app.display()
+
+def destGui() :
+	"""Stop showing gui """
+	app.hide()
+
 import gamelogic
 
 #
 # Put GUI code here
 #
+
+
 
 playButton = gui.PushButton(app, text = "Play", command = gamelogic.gameLoop)
 
@@ -12,6 +24,3 @@ playButton = gui.PushButton(app, text = "Play", command = gamelogic.gameLoop)
 # End GUI code here #
 #
 
-def initGui() :
-	""" Start displaying the gui """
-	app.display()
--- gamelogic.py
+++ gamelogic.py
@@ -1,9 +1,12 @@
 import pygame
-from display import app
+from display import app, initGui
 
 print("Game logic works")
 
 	
 def gameLoop() :
 	""" The main game loop! """
-	app.hide()
+	destGui()
+
+	
+
