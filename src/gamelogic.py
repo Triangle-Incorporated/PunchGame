1f924d318c1383306fde646711d77772e3916dbf
Beacon Brigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-05-17 14:33:04 +0000

    Game window actually closes and opens

--- src/gamelogic.py
+++ src/gamelogic.py
@@ -9,18 +9,22 @@ print("Game logic works")
 ioevents = []
 trackEvents = True
 
+# Window size
+width = 800
+height = 450
+
+pygame.init()
+
 from handleio import checkInputs, ledOutput
 
 def gameLoop() :
 	""" The main game loop! """
-	pygame.init()
 	destGui()
-	
-	screen = pygame.display.set_mode((800, 450))
+	running = True
+
+	screen = pygame.display.set_mode((width, height))
 	pygame.display.set_caption("Punch Game!")
 	background = pygame.image.load("resource/mortal.png")
-	
-	running = True
 
 	# Main game loop
 	while running :
@@ -29,10 +33,13 @@ def gameLoop() :
 		screen.blit(background, (0, 0))
 
 		for event in pygame.event.get() :
-			print("hi")
+			
+			if event.type == pygame.QUIT :
+				running = False
 
 		pygame.display.update()
 
 	# Close game loop
-	initGui()
 	trackEvents = False
+	pygame.display.set_mode((1,1))
+	initGui()
