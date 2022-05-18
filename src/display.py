493636da415e6bba7dfb3c0dae8cfc9676653451
Ryan <100320298+BeaconBrigade@users.noreply.github.com>
2022-05-18 12:33:22 +0000

    comments

--- src/display.py
+++ src/display.py
@@ -18,10 +18,13 @@ import gamelogic
 # Put GUI code here
 #
 
+# Title
 b_title = gui.Box(app, layout = "grid")
 t_title = gui.Text(b_title, text = "Welcome to Punch Game!", grid = [0, 0])
 btn_title = gui.PushButton(b_title, text = "Quit", command = app.destroy, grid = [1, 0])
 
+
+# Play
 playButton = gui.PushButton(app, text = "Play", command = gamelogic.gameLoop)
 
 #
